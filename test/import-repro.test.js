816fc96a609f73a482078a1eb0926f7832bf27fd
perpil <97474956+perpil@users.noreply.github.com>
2024-10-06 12:17:21 -0700

    Initial commit

---
+++ test/import-repro.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const ImportRepro = require('../lib/import-repro-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/import-repro-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new ImportRepro.ImportReproStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
